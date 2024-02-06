62a359d8e12d20dd622f803ca341a56040a1dffc
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-02-07 04:15:04 +0500

    add catalog

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ const menuBtn = document.querySelector(".menu");
 menuBtn.addEventListener("click", () => {
   header.classList.toggle("open");
 });
+
+let modeBtn = document.getElementById("catalog");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("catalog-open");
+});
