be47213f9fd776283e9667a7bc0577d526961293
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-02-09 00:33:54 +0500

    add categorias page

--- js/products.js
+++ js/products.js
@@ -1,3 +1,4 @@
+// NAVBAR BURGER
 const header = document.querySelector("header");
 const menuBtn = document.querySelector(".menu");
 
@@ -5,8 +6,36 @@ menuBtn.addEventListener("click", () => {
   header.classList.toggle("open");
 });
 
+// CATALOG
+
 let modeBtn = document.getElementById("catalog");
 
 modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("catalog-open");
+});
+
+// MODAL
+const btnOpen = document.getElementById("btn-open");
+const btnClose = document.getElementById("btn-close");
+const btnSubmit = document.getElementById("submit");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+
+btnOpen.addEventListener("click", function (e) {
+  modal.classList.toggle("open");
+  // document.body.style.overflow = "hidden";
+});
+
+btnClose.addEventListener("click", function (e) {
+  modal.classList.remove("open");
+  document.body.style.overflow = "auto";
+});
+
+modalContent.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+modal.addEventListener("click", function (e) {
+  modal.classList.remove("open");
+  document.body.style.overflow = "auto";
 });
